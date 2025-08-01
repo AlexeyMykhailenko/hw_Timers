de8c52ef0642d36dedd7ab1bdeb57637137a315f
Oleksii Mykhailenko <157206927+AlexeyMykhailenko@users.noreply.github.com>
2025-08-02 01:40:12 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,97 +1,97 @@
-// const timerOutput = document.getElementById("timer-output");
-// const timerStartBtn = document.getElementById("timer-start-btn");
-// const timerStopBtn = document.getElementById("timer-stop-btn");
-// const warning = document.getElementById("warning");
-// let timerId = null;
-// let hours = 1;
-// let minutes = 0;
-// let seconds = 0;
+const timerOutput = document.getElementById("timer-output");
+const timerStartBtn = document.getElementById("timer-start-btn");
+const timerStopBtn = document.getElementById("timer-stop-btn");
+const warning = document.getElementById("warning");
+let timerId = null;
+let hours = 1;
+let minutes = 0;
+let seconds = 0;
 
-// timerOutput.textContent = "01:00:00";
+timerOutput.textContent = "01:00:00";
 
-// timerStartBtn.addEventListener("click", timerLoop);
-// timerStopBtn.addEventListener("click", stopTimer);
+timerStartBtn.addEventListener("click", timerLoop);
+timerStopBtn.addEventListener("click", stopTimer);
 
-// function timeFormatter(time) {
-//     return time.toString().padStart(2, "0");
-// };
+function timeFormatter(time) {
+    return time.toString().padStart(2, "0");
+};
 
-// function updateTimer() {
-//     timerOutput.textContent = `${timeFormatter(hours)}:${timeFormatter(minutes)}:${timeFormatter(seconds)}`;
-// };
+function updateTimer() {
+    timerOutput.textContent = `${timeFormatter(hours)}:${timeFormatter(minutes)}:${timeFormatter(seconds)}`;
+};
 
-// function timerWarning() {
-//     warning.textContent = `Only ${minutes} minutes left!`;
-//     warning.style.display = "block";
-// };
+function timerWarning() {
+    warning.textContent = `Only ${minutes} minutes left!`;
+    warning.style.display = "block";
+};
 
-// updateTimer();
+updateTimer();
 
-// function timerLoop() {
-//     if (timerId !== null) return;
+function timerLoop() {
+    if (timerId !== null) return;
 
-//     hours = 1;
-//     minutes = 0;
-//     seconds = 0;
+    hours = 1;
+    minutes = 0;
+    seconds = 0;
 
-//     updateTimer();
+    updateTimer();
 
-//     timerId = setInterval(() => {
+    timerId = setInterval(() => {
 
-//         if (hours === 0 && minutes === 0 && seconds === 0) {
-//             clearInterval(timerId);
-//             timerId = null;
-//             hours = 1;
-//             minutes = 59;
-//             seconds = 59;
+        if (hours === 0 && minutes === 0 && seconds === 0) {
+            clearInterval(timerId);
+            timerId = null;
+            hours = 1;
+            minutes = 59;
+            seconds = 59;
 
-//             setTimeout(() => {
-//                 timerOutput.textContent = `${hoursOutput}:${minutesOutput}:${secondsOutput}`;
-//                 return;
-//             }, 2000);
+            setTimeout(() => {
+                timerOutput.textContent = `${hoursOutput}:${minutesOutput}:${secondsOutput}`;
+                return;
+            }, 2000);
 
-//             timerOutput.textContent = "Time is up!";
-//         };
+            timerOutput.textContent = "Time is up!";
+        };
 
-//         if (hours < 1 && minutes <= 58) {
-//             timerWarning();
-//         };
+        if (hours < 1 && minutes <= 58) {
+            timerWarning();
+        };
 
-//         if (seconds === 0) {
-//             if (minutes === 0) {
-//                 if (hours > 0) {
-//                     hours--;
-//                     minutes = 59;
-//                     seconds = 59;
-//                 }
-//             }
-//             else {
-//                 minutes--;
-//                 seconds = 59;
-//             }
-//         }
-//         else {
-//             seconds--;
-//         };
-
-
-//         updateTimer();
-
-//     }, 1000);
-// };
-
-// function stopTimer() {
-//     if (timerId === null) return;
-
-//     clearInterval(timerId);
-//     timerId = null;
-//     hours = 1;
-//     minutes = 0;
-//     seconds = 0;
+        if (seconds === 0) {
+            if (minutes === 0) {
+                if (hours > 0) {
+                    hours--;
+                    minutes = 59;
+                    seconds = 59;
+                }
+            }
+            else {
+                minutes--;
+                seconds = 59;
+            }
+        }
+        else {
+            seconds--;
+        };
 
 
-//     updateTimer();
-// };
+        updateTimer();
+
+    }, 1000);
+};
+
+function stopTimer() {
+    if (timerId === null) return;
+
+    clearInterval(timerId);
+    timerId = null;
+    hours = 1;
+    minutes = 0;
+    seconds = 0;
+
+
+    updateTimer();
+};
 
 const stopwatchOutput = document.getElementById("stopwatch-output");
 const stopwatchStartBtn = document.getElementById("stopwatch-start-btn");
